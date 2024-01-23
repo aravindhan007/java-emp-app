068fd2a7c9bddf4bfc8311c0cf92c7385fbaf3f5
M <maravin@apac.corpdir.net>
2024-01-23 14:57:52 +0530

    first commit

---
+++ springbootbackend/src/main/java/com/example/springbootbackend/SpringbootbackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbackendApplication.class, args);
+		
+	}
+
+}
